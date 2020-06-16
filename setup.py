854d78f5e3e02349fef4f4ecc0438defd0265a7c
Pavel Vorobyov <p.vorobyov@corp.mail.ru>
2020-06-16 14:06:17 +0300

    initial

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="uenginecli",
+    version="1.0.0",
+    description="a helper client library for accessing uengine-based APIs",
+    url="https://github.com/viert/uenginecli",
+    author="Pavel Vorobyov",
+    author_email="aquavitale@yandex.ru",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+    ]
+)
